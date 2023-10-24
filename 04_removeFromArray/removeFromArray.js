5b95f07e51d5402b10871571444a4e2044503807
Jason Paris <paris3200@gmail.com>
2023-10-23 20:49:07 -0400

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+  let filteredArray = [];
+  for (const removeItem of args) {
+    removeIndex = array.indexOf(removeItem);
+    if (removeIndex > -1){
+      array.splice(removeIndex, 1);
+    };
+    filteredArray = array;
+  };
+  return filteredArray;
 };
 
 // Do not edit below this line
