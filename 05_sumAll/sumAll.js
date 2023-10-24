5a23205212122cb2d3bffaa331b908aa833ce1a3
Jason Paris <paris3200@gmail.com>
2023-10-23 21:04:47 -0400

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+  if (
+    startInt < 0 ||
+    endInt < 0 ||
+    typeof startInt != 'number' ||
+    typeof endInt != 'number'
+  )
+    return 'ERROR'
+
+  let lowerInt = Math.min(startInt, endInt);
+  let upperInt = Math.max(startInt, endInt);
+
+  sum = 0;
+  for (let x = lowerInt; x <= upperInt; x++){
+    sum = sum + x;
+  };
+
+  return sum;
 
 };
 
