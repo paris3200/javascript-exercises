bbe3b459fcec57305f463015e24996cb4b70513a
Jason Paris <paris3200@gmail.com>
2023-10-23 20:31:33 -0400

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+  let stringArray = inputString.split("");
+  let reversedArray = stringArray.reverse();
+  let reversedString =  reversedArray.join("")
+  return reversedString
 };
 
 // Do not edit below this line
