f647dcfe0395415323c72c99072b46b8b5d46982
Jason Paris <paris3200@gmail.com>
2023-10-26 16:27:27 -0400

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const ordered = people.sort(function (firstPerson, secondPerson) {
+      if (!("yearOfDeath" in firstPerson)){
+        firstPerson.yearOfDeath = new Date().getFullYear();
+      };
+      if (!("yearOfDeath" in secondPerson)){
+        secondPerson.yearOfDeath = new Date().getFullYear();
+      };
+      firstPersonAge = firstPerson.yearOfDeath - firstPerson.yearOfBirth;
+      secondPersonAge = secondPerson.yearOfDeath - secondPerson.yearOfBirth;
+        
+        if (firstPersonAge > secondPersonAge) {
+          return -1;
+        } else {
+          return 1;
+        };
+      });
+
+    return ordered[0]
 
 };
 
