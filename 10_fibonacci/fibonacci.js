d81f710e080d565c6e10ad06b8d6dfd737350726
Jason Paris <paris3200@gmail.com>
2023-10-26 15:59:31 -0400

    Solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(num) {
+  if (isNaN(num) == true || num < 0){
+    return "OOPS"
+  };
+  let n1 = 1;
+  let n2 = 0;
+  for (let x=1; x < num; x += 1){
+    let n = n1 + n2;
+    n2 = n1;
+    n1 = n;
+  };
+  return n1;
+  }
+  
 // Do not edit below this line
 module.exports = fibonacci;
