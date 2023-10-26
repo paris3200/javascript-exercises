9dc447febd322c940fb4862e894545040e501a66
Jason Paris <paris3200@gmail.com>
2023-10-25 21:59:46 -0400

    Add feature sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,12 @@ const subtract = function(a, b) {
   return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function( array) {
+  let sum = 0;
+  for (const item of array){
+    sum += item;
+  }
+  return sum
 };
 
 const multiply = function() {
