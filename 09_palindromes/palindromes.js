e320393f11ed9af4dbabecb508d21e1017dcaddb
Jason Paris <paris3200@gmail.com>
2023-10-26 07:30:36 -0400

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (original) {
+  let stripSpaces = original.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/ /g, "");
+  filteredString = stripSpaces.toLowerCase();
 
+  let reversedString = filteredString.split("").reverse().join("")
+
+  if (filteredString == reversedString){
+    return true;
+  }
+  return false;
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
